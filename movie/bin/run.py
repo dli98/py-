c3474e2f52ba18a3fcd1a58d8d326a9f72245908
wkd-lidashuang <42607425+wkd-lidashuang@users.noreply.github.com>
2018-09-26 09:22:46 +0800

    movie
    豆瓣电影和以及评论

---
+++ movie/bin/run.py
@@ -0,0 +1,6 @@
+# 程序文件入口 开始文件
+import os
+import sys
+sys.path.append(os.path.dirname(os.getcwd()))
+
+from core import schedule
---
+++ movie/core/schedule.py
@@ -0,0 +1,45 @@
+from concurrent.futures import ThreadPoolExecutor
+from core.movie import hot_movie
+from conf.setting import Crawl_page
+from core.movie_comment import get_comment
+from core.mongodb import MongoDB_Client
+
+
+class Schedule:
+    @staticmethod
+    def get_hot_movie():
+        """
+        获得热门电影的id等信息
+        :return:
+        """
+        print('正在获得热门电影')
+        tpool = ThreadPoolExecutor(20)
+        for x in range(Crawl_page):
+            tpool.submit(hot_movie, x * 20)
+        tpool.shutdown()
+
+    @staticmethod
+    def parse_comment():
+        """
+        获得所有评论
+        :return:
+        """
+        print('正在获得热门电影所对应的评论')
+        c = MongoDB_Client()
+        infos = c.read_mongo({}, {'_id': 0, 'id': 1, 'title': 1})
+        tpool = ThreadPoolExecutor(20)
+        for info in infos:
+            name = info.get('title')
+            id_ = info.get('id')
+            tpool.submit(get_comment, name, id_)
+        tpool.shutdown()
+
+    def run(self):
+        print('Movie processing runing')
+        movie_id_process = Schedule.get_hot_movie()
+        comment_process = Schedule.parse_comment()
+
+
+if __name__ == '__main__':
+    s = Schedule()
+    s.run()
