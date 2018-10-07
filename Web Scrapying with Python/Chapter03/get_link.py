dc7157682f2642ac9ed89cc2d31690c67fdef819
wkd-lidashuang <lidashuang_wkd@163.com>
2018-10-07 18:45:41 +0800

    add Chapter03

---
+++ Web Scrapying with Python/Chapter03/get_link.py
@@ -0,0 +1,24 @@
+from urllib.request import  urlopen
+
+from bs4 import  BeautifulSoup
+
+import  datetime
+import  random
+import  re
+
+
+random.seed(datetime.datetime.now())
+
+def getLinks(articeUrl):
+    html = urlopen("http://en.wikipedia.org" + articeUrl )
+    data = BeautifulSoup(html, "html.parser")
+
+    return  data.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+links = getLinks("/wiki/Kevin_Bacon")
+
+while len(links) > 0:
+    newArticle = links[random.randint(0,len(links)-1)].attrs['href']
+    print(newArticle)
+
+    links = getLinks(newArticle)
