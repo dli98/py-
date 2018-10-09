350303f4ea11d4c4eb980951f7b6283eb2ead9a9
wkd-lidashuang <lidashuang_wkd@163.com>
2018-10-09 10:47:11 +0800

    Markov text generator

---
+++ Web Scrapying with Python/Chapter08/MarkovGenerator.py
@@ -0,0 +1,82 @@
+from urllib.request import urlopen
+from random import randint
+import collections
+
+
+def wordListSum(wordList):
+    sum = 0
+    for word, value in wordList.items():
+        sum += value
+    return sum
+
+
+def retrieveRandomWord(wordList):
+    randIndex = randint(1, wordListSum(wordList))
+    for word, value in wordList.items():
+        randIndex -= value
+        if randIndex <= 0:
+            return word
+
+
+def buildWordDict(text):
+    # Remove newlines and quotes
+    text = text.replace("\n", " ")
+    text = text.replace("\"", "")
+
+    # Make sure puncuation are treated as their own "word," so they will be included
+    # in the Markov chain
+    punctuation = [',', '.', ';', ':']
+    for symbol in punctuation:
+        text = text.replace(symbol, " " + symbol + " ")
+
+    words = text.split(" ")
+    # Filter out empty words
+    words = [word for word in words if word != ""]
+
+    wordDict = {}
+    for i in range(1, len(words)):
+        if words[i - 1] not in wordDict:
+            # Create a new dictionary for this word
+            wordDict[words[i - 1]] = {}
+        if words[i] not in wordDict[words[i - 1]]:
+            wordDict[words[i - 1]][words[i]] = 0
+        wordDict[words[i - 1]][words[i]] += 1
+
+    """
+    # defaultdict: 
+    
+    wordDict = collections.defaultdict(dict)
+    for i in range(1, len(words)):
+        if words[i] not in wordDict[words[i - 1]]:
+            wordDict[words[i - 1]][words[i]] = 0
+        wordDict[words[i - 1]][words[i]] += 1
+    """
+
+    """
+        # setdefault: 
+        
+        
+        wordDict = {}
+        for i in range(1, len(words)):
+            if words[i] not in wordDict.setdefault(words[i - 1], {}):
+                wordDict[words[i - 1]][words[i]] = 0
+            wordDict[words[i - 1]][words[i]] += 1
+    """
+
+    return wordDict
+
+
+text = str(urlopen("https://pythonscraping.com/files/inaugurationSpeech.txt").read(), 'utf-8')
+wordDict = buildWordDict(text)
+# print(wordDict)
+
+# Generate a Markov chain of length 100
+length = 100
+chain = ""
+currentWord = "I"
+for i in range(0, length):
+    chain += currentWord + " "
+    # print(wordDict[currentWord])
+    currentWord = retrieveRandomWord(wordDict[currentWord])
+
+print(chain)
